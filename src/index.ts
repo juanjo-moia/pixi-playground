7ff11dd544fb8dc87e4674977eadb8e8b1f71233
Carina Siebelt <carina.siebelt@moia.io>
2022-04-26 14:05:42 +0200

    first attempt to tint the background :D

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js';
+import { Application, Container, Sprite, Texture } from 'pixi.js';
 
 const WIDTH = 400;
 const HEIGHT = 300;
@@ -12,6 +12,23 @@ const app = new Application({
 });
 document.body.appendChild(app.view);
 
+/** background **/
+const bgContainer = new Container();
+app.stage.addChild(bgContainer);
+
+const dayBg = new Sprite(Texture.WHITE);
+dayBg.tint = 0x03a9f4;
+bgContainer.addChild(dayBg)
+
+const nightBg = new Sprite(Texture.WHITE);
+nightBg.tint = 0x02163b;
+nightBg.alpha = 0;
+bgContainer.addChild(nightBg)
+
+bgContainer.width = dayBg.width = nightBg.width = WIDTH;
+bgContainer.height = dayBg.height = nightBg.height = HEIGHT;
+
+/** rocket */
 const rocket = Sprite.from('assets/rocket.png');
 app.stage.addChild(rocket);
 rocket.position.set(WIDTH / 2 - rocket.width / 2, HEIGHT + rocket.height);
@@ -23,4 +40,6 @@ app.ticker.add((dt) => {
   const x2 = rocket.position.x;
   const y2 = rocket.position.y - ROCKET_SPEED * dt;
   rocket.position.set(x2, y2);
+
+  nightBg.alpha += 0.003 * dt;
 });
